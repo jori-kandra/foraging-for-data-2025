2e2f427405818cd42912fce9d8542d474f023953
jori-kandra <jori.kandra@gmail.com>
2025-08-26 14:05:39 -0400

    8/26 practice

--- code/live-demo-practice.R
+++ code/live-demo-practice.R
@@ -2,18 +2,20 @@
 library(tidyverse)
 library(data.table) # setnames() - map data dictionary onto raw data
 library(lubridate) # helper functions to handle date formats
-library(openxlsx2) # map data to excel wb
+library(openxlsx2) # create excel wbs
 
 # download raw data
 system("wget https://oui.doleta.gov/unemploy/csv/ar539.csv -P input/")
 
-# data dictionary
+# import data dictionary
 data_dictionary <- read.csv("input/eta539_var_names.csv")
 
-# raw data
+# load raw data
 raw_data <- read.csv("input/ar539.csv") |> 
   setnames(old = data_dictionary$dol_code, new = data_dictionary$dol_title) |> 
-  mutate(report_date = mdy(report_date))
+  mutate(report_date = mdy(report_date),
+         reflect_week_ending = mdy(reflect_week_ending))
+
 
 # initial claims
 initial_claims <- raw_data |> 
@@ -21,19 +23,20 @@ initial_claims <- raw_data |>
   select(report_date, state, nsa_initial_claims) |> 
   pivot_wider(id_cols = report_date, names_from = state, values_from = nsa_initial_claims) |> 
   setnames(old = state.abb, new = state.name) |> 
-  rename(`District of Columbia` = DC) |> 
-  select(-PR, -VI)
+  select(-PR, -VI) |>
+  rename(`District of Columbia` = DC)
+
 
 # continued claims
 continued_claims <- raw_data |> 
   mutate(nsa_continued_claims = state_ui_adjusted_continued_weeks_claimed + stc_workshare_equivalent_continued_weeks_claimed) |> 
-  select(report_date, state, nsa_continued_claims) |> 
-  pivot_wider(id_cols = report_date, names_from = state, values_from = nsa_continued_claims) |> 
+  select(reflect_week_ending, state, nsa_continued_claims) |> 
+  pivot_wider(id_cols = reflect_week_ending, names_from = state, values_from = nsa_continued_claims) |> 
   setnames(old = state.abb, new = state.name) |> 
-  rename(`District of Columbia` = DC) |> 
-  select(-PR, -VI)
+  select(-PR, -VI) |>
+  rename(`District of Columbia` = DC)
 
-# export data
+# write excel workbook
 wb <- wb_workbook()
 
 wb$
